f90c5f9cf571a0783660d2d24698ae71e62ea4e6
nhandhi <frostylord8@gmail.com>
2025-02-04 09:53:19 -0500

    introduction to javascript in class examples

---
+++ csce242/242exercises/04_js_intro/script-arrow-functions.js
@@ -0,0 +1,10 @@
+
+//version one where we learn arrow functions
+const doStuff = () => { //the variable is equal to the parameters, after that is what is excuted 
+    console.log("doing stuff");
+}
+
+//do stuff when button clicks
+document.getElementById("btn-click-me").onclick = doStuff;
+//when you take out the () it does it on click
+// () means now
